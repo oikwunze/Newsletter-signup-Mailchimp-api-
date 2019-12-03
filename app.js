487ac762c6f1db8857667ea59a0ade4bb9429afe
ikwunze <okechukwuikwunze@yahoo.com>
2019-12-03 11:32:17 -0600

    My initial Newsletter Signup using mailchimp api

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/signup.html"));
+
+app.post("/", (req, res) => {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    member: [
+      {
+        email_address: email,
+        status: "subscribed"
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/schema/3.0/Lists/0097d0a58c",
+    method: "POST",
+    headers: {
+      Authorization: "Oikwunze b2895bcdb05bd93420cc1f4fc14e9ad2-us4"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      res.sendFile(__dirname + "/success.html");
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// api key b2895bcdb05bd93420cc1f4fc14e9ad2-us4
+
+// list 7b80f67abe
